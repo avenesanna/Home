1f096a83b4712242f2c3171e541e00f2eaccd70e
DESKTOP-L2ALHFK\Анна <aavenes@gmail.com>
2018-10-21 14:48:34 +0300

    no message

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+            var d = b * b - 4 * a * c;
+            var x1 = (-b - Math.Sqrt(d)) / (2 * a);
+            var x2 = (-b + Math.Sqrt(d)) / (2 * a);
+            Console.WriteLine("x1={0}, x2={1}", x1, x2);
+            Console.ReadKey();
+        }
+    }
+}
